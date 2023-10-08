50f591df2979a6fc75122a669f0502b1f30b545f
BBerkinT <topaloglubatinberkin@gmail.com>
2023-10-08 21:18:46 +0300

    Day 1 homework

---
+++ dart_dersleri/lib/degiskenler/odev1.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  String ilce = "Silivri";
+  var kita = "Avrupa";
+  int kargoTakipNo = 24586752;
+  double ekranBoyutu = 15.6;
+  String bilgisayarModeli = "MSI Cyborg 15 AVF";
+
+  print(
+      "Bilgisayarınızın Modeli : $bilgisayarModeli ve Ekran Boyutu : $ekranBoyutu dır.");
+  print(
+      "İstanbul'un $kita Kıtasındaki $ilce İlçesinin Selimpaşa Mahallesine $kargoTakipNo nuamarası ile kargolanmıştır.");
+
+  var otobusHatti = "34T";
+  double hareketSaati = 14.20;
+  String mahalleAdi = "Dolmabahçe";
+
+  print(
+      "$mahalleAdi Mahallesine gidecek olan $otobusHatti Numaralı hattımız saat $hareketSaati'de Taksim'den hareket edecektir.");
+}
