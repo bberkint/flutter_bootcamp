65487f9e1169ee17b30980b8a3194d93718ec760
BBerkinT <topaloglubatinberkin@gmail.com>
2023-10-18 15:48:20 +0300

    Day 3 upcasting, downcasting

--- dart_dersleri/lib/object_oriented_programming/kalitim/kalitim_main.dart
+++ dart_dersleri/lib/object_oriented_programming/kalitim/kalitim_main.dart
@@ -1,3 +1,4 @@
+import 'package:dart_dersleri/object_oriented_programming/kalitim/Ev.dart';
 import 'package:dart_dersleri/object_oriented_programming/kalitim/Saray.dart';
 import 'package:dart_dersleri/object_oriented_programming/kalitim/Villa.dart';
 
@@ -9,4 +10,20 @@ void main(List<String> args) {
   var bogazVilla = Villa(garajVarMi: true, pencereSayisi: 50);
   print(bogazVilla.garajVarMi);
   print(bogazVilla.pencereSayisi);
+
+  //Tip kontrolü
+  if (bogazVilla is Saray) {
+    // instance of
+    print("Saraydır");
+  } else {
+    print("Saray değildir");
+  }
+
+  //Downcasting
+  var ev = Ev(pencereSayisi: 10);
+  var saray = ev as Saray;
+
+  //Upcasting
+  var s = Saray(kuleSayisi: 3, pencereSayisi: 100);
+  Ev e = s;
 }
