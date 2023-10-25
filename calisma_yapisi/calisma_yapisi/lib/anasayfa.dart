81b681c7ed58cbb4af57d4653e88f9b0429fcb63
BBerkinT <topaloglubatinberkin@gmail.com>
2023-10-25 16:18:31 +0300

    Day 5 StatefulWidget, SetState

---
+++ calisma_yapisi/calisma_yapisi/lib/anasayfa.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  int sayac = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print("build() metodu çalıştı");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Anasayfa"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Sonuç : $sayac"),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  sayac += 1;
+                });
+              },
+              child: const Text("Tıkla"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
