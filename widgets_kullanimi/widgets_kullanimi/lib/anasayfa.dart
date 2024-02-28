4d7b102f1d9e68b33bca67f15dfe9888923b4b7c
BBerkinT <topaloglubatinberkin@gmail.com>
2024-02-28 23:57:09 +0300

    Day 6

--- widgets_kullanimi/widgets_kullanimi/lib/anasayfa.dart
+++ widgets_kullanimi/widgets_kullanimi/lib/anasayfa.dart
@@ -7,11 +7,17 @@ class Anasayfa extends StatefulWidget {
   State<Anasayfa> createState() => _AnasayfaState();
 }
 
-// Day 6 Codes
 class _AnasayfaState extends State<Anasayfa> {
   var tfController = TextEditingController();
   String alinanVeri = "";
   String resimAdi = "mood.png";
+  bool switchKontrol = false;
+  bool checkboxKontrol = false;
+  int radioDeger = 0;
+  bool progressKontrol = false;
+  double ilerleme = 30.0;
+  var tfSaat = TextEditingController();
+  var tfTarih = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -63,6 +69,163 @@ class _AnasayfaState extends State<Anasayfa> {
                   child: Text("Resim 2"),
                 ),
               ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: SwitchListTile(
+                    title: const Text("Dart"),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: switchKontrol,
+                    onChanged: (veri) {
+                      setState(() {
+                        switchKontrol = veri;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: CheckboxListTile(
+                    title: const Text("Flutter"),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: checkboxKontrol,
+                    onChanged: (veri) {
+                      setState(() {
+                        checkboxKontrol = veri!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 200,
+                  child: RadioListTile(
+                    title: const Text("Galatasaray"),
+                    value: 1,
+                    groupValue: radioDeger,
+                    onChanged: (veri) {
+                      setState(() {
+                        radioDeger = veri!;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: RadioListTile(
+                    title: const Text(
+                      "Real Madrid",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    value: 2,
+                    groupValue: radioDeger,
+                    onChanged: (veri) {
+                      setState(() {
+                        radioDeger = veri!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      progressKontrol = true;
+                    });
+                  },
+                  child: Text("Başla"),
+                ),
+                Visibility(
+                    visible: progressKontrol,
+                    child: const CircularProgressIndicator()),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      progressKontrol = false;
+                    });
+                  },
+                  child: Text("Dur"),
+                ),
+              ],
+            ),
+            Text(ilerleme.toInt().toString()),
+            Slider(
+              max: 100.0,
+              min: 0.0,
+              value: ilerleme,
+              onChanged: (veri) {
+                setState(() {
+                  ilerleme = veri;
+                });
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 120,
+                  child: TextField(
+                    controller: tfSaat,
+                    decoration: const InputDecoration(
+                      hintText: "Saat",
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.fromDateTime(DateTime.now()))
+                        .then((value) {
+                      tfSaat.text = "${value!.hour} : ${value.minute}";
+                    });
+                  },
+                  icon: const Icon(Icons.access_time),
+                ),
+                SizedBox(
+                  width: 120,
+                  child: TextField(
+                    controller: tfTarih,
+                    decoration: const InputDecoration(
+                      hintText: "Tarih",
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(2000),
+                        lastDate: DateTime(2030))
+                      ..then((value) {
+                        tfTarih.text =
+                            "${value!.day} / ${value.month} / ${value.year}";
+                      });
+                  },
+                  icon: const Icon(Icons.date_range),
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("Switch durum : $switchKontrol");
+                print("CheckBox durum : $checkboxKontrol");
+                print("Radio Durum : $radioDeger");
+                print("Slider Durum : $ilerleme");
+              },
+              child: Text("Göster"),
             ),
           ],
         ),
