37a49664369a7e4fac3eac80e48e0858c215af81
BBerkinT <topaloglubatinberkin@gmail.com>
2024-03-07 19:21:57 +0300

    Day 10 Filmler Uygulaması SQLite

--- filmler_app/filmler_app/lib/data/repo/filmlerdao_repository.dart
+++ filmler_app/filmler_app/lib/data/repo/filmlerdao_repository.dart
@@ -1,26 +1,20 @@
 import 'package:filmler_app/data/entity/filmler.dart';
+import 'package:filmler_app/sqlite/veritabani_yardimcisi.dart';
 
 class FilmlerDaoRepository {
   Future<List<Filmler>> filmleriYukle() async {
-    var filmlerListesi = <Filmler>[];
+    var db = await VeritabaniYardimcisi.veritabaniErisim();
 
-    var f1 = Filmler(id: 1, ad: "Django", resim: "django.png", fiyat: 24);
-    var f2 = Filmler(
-        id: 2, ad: "Interstellar", resim: "interstellar.png", fiyat: 32);
-    var f3 = Filmler(id: 3, ad: "Inception", resim: "inception.png", fiyat: 14);
-    var f4 = Filmler(
-        id: 4, ad: "The Hateful", resim: "thehatefuleight.png", fiyat: 36);
-    var f5 =
-        Filmler(id: 5, ad: "The Pianist", resim: "thepianist.png", fiyat: 72);
-    var f6 = Filmler(id: 6, ad: "Anadoluda", resim: "anadoluda.png", fiyat: 86);
+    List<Map<String, dynamic>> maps =
+        await db.rawQuery("SELECT * FROM filmler");
 
-    filmlerListesi.add(f1);
-    filmlerListesi.add(f2);
-    filmlerListesi.add(f3);
-    filmlerListesi.add(f4);
-    filmlerListesi.add(f5);
-    filmlerListesi.add(f6);
-
-    return filmlerListesi;
+    return List.generate(maps.length, (i) {
+      var satir = maps[i];
+      return Filmler(
+          id: satir["id"],
+          ad: satir["ad"],
+          resim: satir["resim"],
+          fiyat: satir["fiyat"]);
+    });
   }
 }
