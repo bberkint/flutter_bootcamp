97257915e6a9d324cec70ffb297111f54d0aff37
BBerkinT <topaloglubatinberkin@gmail.com>
2024-03-09 23:42:57 +0300

    Day 13 Konum Alma

---
+++ lokasyon_kullanimi/lib/anasayfa.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  double enlem = 0.0;
+  double boylam = 0.0;
+
+  Future<void> konumBilgisiAl() async {
+    var konum = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      enlem = konum.latitude;
+      boylam = konum.longitude;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Konum Kullanımı"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Enlem : $enlem", style: TextStyle(fontSize: 30)),
+            Text("Boylam : $boylam", style: TextStyle(fontSize: 30)),
+            ElevatedButton(
+                onPressed: () {
+                  konumBilgisiAl();
+                },
+                child: Text("Konum Al")),
+          ],
+        ),
+      ),
+    );
+  }
+}
